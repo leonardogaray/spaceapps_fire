3ab1bd49858b3ceabdf19277ff345feb107c9a38
leonardogaray <leonardo.andres.garay@gmail.com>
2023-10-07 15:17:45 -0300

    222

--- spaceapps_fire/src/app/tab1/tab1.page.ts
+++ spaceapps_fire/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import * as L from 'leaflet';
 
 @Component({
@@ -12,6 +13,8 @@ export class Tab1Page implements AfterViewInit{
   private marker: any;
   private lat: number = -33.27688650546989;
   private lng: number = -59.820610521170714;
+
+  constructor(private httpClient: HttpClient) { }
 
   private initMap(): void {
     this.marker = L.marker([this.lat, this.lng]);
@@ -32,8 +35,10 @@ export class Tab1Page implements AfterViewInit{
     this.marker.addTo(this.map)
 
     setTimeout(() => { 
-      this.map.invalidateSize()
+      this.map.invalidateSize();
     }, 500 );
+
+    this.loadData();
   }
 
   ngAfterViewInit(): void {
@@ -45,8 +50,69 @@ export class Tab1Page implements AfterViewInit{
     },function(){
         alert('User not allowed')
     },{timeout:10000});
+  }
 
-    
+  loadData(): void{
+    let url = "https://firms.modaps.eosdis.nasa.gov/api/country/csv/76db7f998faeaeb1d02f68b315bb5a12/VIIRS_SNPP_NRT/ARG/1/2023-10-07";
+    const headers = new HttpHeaders({
+        Accept: 'text/csv',
+    });
+
+    this.httpClient
+      .get(url, {headers, responseType: 'text'})
+      .subscribe(response => {
+        let jsonData = this.csv2Json(response);
+        let coordinates = this.processData(jsonData);
+        this.generateFires(coordinates);
+      });
+  }
+
+  private generateFires(coordinates: any[]): void {
+    let self = this;
+    coordinates.forEach(function(coordinate: any){
+      L.circle([coordinate.latitude, coordinate.longitude], {
+        color: 'red',
+        fillColor: 'red',
+        fillOpacity: 0.5,
+        radius: 1000
+      }).addTo(self.map);
+    });
+  }
+
+  private csv2Json(csv: string): any[] {
+    let lines = csv.split("\n");
+    let result: any = [];
+    let headers = lines[0].split(",");
+    for (let i = 1; i < lines.length - 1; i++) {
+        let obj:any = {};
+        let currentline = lines[i].split(",");
+        for (let j = 0; j < headers.length; j++) {
+            obj[headers[j]] = currentline[j];
+        }
+        result.push(obj);
+    }
+    return result;
+  }
+
+  private isCoordinateInsideRectangle(
+    lat: number, lon: number, minLat: number, maxLat: number, minLon: number, maxLon: number) {
+      return lat >= minLat && lat <= maxLat && lon >= minLon && lon <= maxLon;
+  }
+
+  private processData(lines: any) {
+      const minLat:number = -34.53191815752273;
+      const maxLat:number = -31.7585755065532;
+      const minLon:number = -61.026920398474445;
+      const maxLon:number = -58.20905577137098;
+
+      const filteredFires = lines
+          .filter((fire: any) => {
+              const lat = parseFloat(fire.latitude);
+              const lon = parseFloat(fire.longitude);
+              return this.isCoordinateInsideRectangle(lat, lon, minLat, maxLat, minLon, maxLon);
+          });
+
+      return filteredFires;
   }
 
 }
